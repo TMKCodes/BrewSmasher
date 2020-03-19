b036e8757917b26b3b8b875d490529b904854f04
TMKCodes <tmkcodes@gmx.com>
2020-03-19 09:24:10 +0200

    remove deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#ssh -t tonto@157.230.22.53 "echo -e 'ikaros123\nikaros123\n' | sudo chown -R tonto:tonto ~/brewsmasher"
-rsync -r ./ tonto@157.230.22.53:~/brewsmasher --no-group --no-perms --no-times
-ssh -t tonto@157.230.22.53 "sudo chown -R www-data:www-data ~/brewsmasher"
